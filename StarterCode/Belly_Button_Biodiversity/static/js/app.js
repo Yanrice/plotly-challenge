9cd915419d6443ebbbea90822260e1df71c88fd6
Yanrice <51466040+Yanrice@users.noreply.github.com>
2019-10-19 22:42:47 -0400

    PLOTLY

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -24,24 +24,53 @@ function buildMetadata(sample) {
        buildGauge(data.WFREQ);
   });
 }
-    
-
-
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
   var url = `/samples/${sample}`;
    d3.json(url).then(function (data){
+      var size_value = response["sample_values"];
+      var label = response["otu_labels"];
       var x_value = response["otu_ids"];
-    var y_value = response["sample_values"];
-    var size_value = response["sample_values"];
-    var label = response["otu_labels"];
+      var y_value = response["sample_values"];
+   
     // @TODO: Build a Bubble Chart using the sample data
+  var traceB = {
+      x: x_value,
+      y: y_value,
+      mode:"markers", 
+      marker:{
+        size: size_value,
+        color: x_value,
+        colorscale: "Rainbow",
+        labels: label,
+        type: 'scatter',
+        opacity: 0.3
+      }
+    };
+  var data1 = [traceB];
 
+  var layout = {
+      title: "Marker Size",
+      xaxis: { title: "OTU ID" },
+      showlegend: true
+    };
+
+Plotly.newPlot('bubble', data, layout);
     // @TODO: Build a Pie Chart
+    var data = [{
+      values: size_value.splice(0, 10),
+      labels: x_value.splice(0, 10),
+      text: y_value.splice(0,10),
+      type: 'pie'
+    }];
+    Plotly.newPlot('pie', data);
+    // @TODO: Build a Pie Chart
+    });
+}
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-}
+
 
 function init() {
   // Grab a reference to the dropdown select element
